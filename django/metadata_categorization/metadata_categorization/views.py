c5d06fc0475fe0c46f6785351b7765f7a4452c7a
Eric Weitz <eric.weitz@nih.gov>
2016-01-05 19:03:55 +0000

    Stash first draft data structure mockup

--- django/metadata_categorization/metadata_categorization/views.py
+++ django/metadata_categorization/metadata_categorization/views.py
@@ -11,5 +11,82 @@ from django.core.urlresolvers import reverse
 
 class IndexView(generic.TemplateView):
     template_name = 'metadata_categorization/index.html'
-	
 
+class QueueView(generic.TemplateView):
+    template_name = 'metadata_categorization/queue.html'
+
+    queue = {
+
+    }
+
+    # curationQueue
+    # ID
+    # list summaryRecord
+    # summaryRecord
+    # list indivdualRecord
+    # list
+    # individualRecord term|cell line|cell type|anatomy|species|disease|note
+
+
+    individualRecords = [
+        {
+            "id": 12,
+            "cell line": {"source": "HeLa", "annotation": ""},
+            "cell type": {"source": "epf tm", "annotation": ""},
+            "cell treatment": {"source": "firboblast", "annotation": ""},
+            "anatomy": {"source": "Folderol", "annotation": ""},
+            "species": {"source": "human", "annotation": ""},
+            "disease": {"source": "gout", "annotation": ""},
+            "note": ""
+        },
+        {
+            "id": 1234,
+            "cell line": {"source": "epf-1", "annotation": ""},
+            "cell type": {"source": "epf tm", "annotation": ""},
+            "cell treatment": {"source": "firboblast", "annotation": ""},
+            "anatomy": {"source": "Folderol", "annotation": ""},
+            "species": {"source": "human", "annotation": ""},
+            "disease": {"source": "gout", "annotation": ""},
+            "note": ""
+        },
+        {
+            "id": 12345,
+            "cell line": {"source": "epf-1", "annotation": ""},
+            "cell type": {"source": "epf tm", "annotation": ""},
+            "cell treatment": {"source": "firboblast", "annotation": ""},
+            "anatomy": {"source": "Folderol", "annotation": ""},
+            "species": {"source": "human", "annotation": ""},
+            "disease": {"source": "gout", "annotation": ""},
+            "note": ""
+        }
+        {
+            "id": 12,
+            "cell line": {"source": "epf-1", "annotation": ""},
+            "cell type": {"source": "epf tm", "annotation": ""},
+            "cell treatment": {"source": "firboblast", "annotation": ""},
+            "anatomy": {"source": "Folderol", "annotation": ""},
+            "species": {"source": "human", "annotation": ""},
+            "disease": {"source": "gout", "annotation": ""},
+            "note": ""
+        },
+        {
+            "id": 123,
+            "cell line": {"source": "epf-1", "annotation": ""},
+            "cell type": {"source": "epf tm", "annotation": ""},
+            "cell treatment": {"source": "firboblast", "annotation": ""},
+            "anatomy": {"source": "Folderol", "annotation": ""},
+            "species": {"source": "human", "annotation": ""},
+            "disease": {"source": "gout", "annotation": ""},
+            "note": ""
+        },
+        {
+            "id": 12345,
+            "cell line": {"source": "epf-1", "annotation": ""},
+            "cell type": {"source": "epf tm", "annotation": ""},
+            "cell treatment": {"source": "firboblast", "annotation": ""},
+            "anatomy": {"source": "Folderol", "annotation": ""},
+            "species": {"source": "human", "annotation": ""},
+            "disease": {"source": "gout", "annotation": ""},
+            "note": ""
+        }
+    ]
