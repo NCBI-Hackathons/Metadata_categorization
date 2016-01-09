f727bac83adde652fafe60ae747d53b73a2d8df6
Eric Weitz <eric.m.weitz@gmail.com>
2016-01-09 23:59:07 +0000

    Added icon to display individual records list

--- django/metadata_categorization/metadata_categorization/static/metadata_categorization/metadata.js
+++ django/metadata_categorization/metadata_categorization/static/metadata_categorization/metadata.js
@@ -3,6 +3,17 @@ $(document).ready(function() {
 
   var container = document.getElementById("queue");
 
+
+  function plusRenderer (instance, td, row, col, prop, value, cellProperties) {
+
+    var plusIcon =
+      '<svg class="icon icon--plus" viewBox="0 0 5 5" height="20px" width="20px">' +
+        '<path d="M2 1 h1 v1 h1 v1 h-1 v1 h-1 v-1 h-1 v-1 h1 z" />' +
+      '</svg>';
+
+    $(td).empty().append(plusIcon);
+  }
+
   var hot = new Handsontable(container, {
     data: summaryRecords,
     height: 396,
@@ -12,27 +23,17 @@ $(document).ready(function() {
     columnSorting: true,
     contextMenu: true,
     colHeaders: [
-      "Submitted cell line", "Cell line", "Cell type", "Anatomy", "Species",
-      "Disease"
+      "", "Submitted cell line", "Cell line", "Cell type", "Anatomy",
+      "Species", "Disease"
     ],
-    columns: [{
-      data: "sourceCellLine",
-      type: "text"
-     }, {
-      data: "annotCellLine",
-      type: "text"
-     }, {
-      data: "sourceCellType",
-      type: "text"
-     }, {
-      data: "sourceCellAnatomy",
-      type: "text"
-     }, {
-      data: "sourceSpecies",
-      type: "text"
-    }, {
-     data: "sourceDisease",
-     type: "text"
-   }]
+    columns: [
+      {data: "", renderer: plusRenderer},
+      {data: "sourceCellLine"},
+      {data: "annotCellLine"},
+      {data: "sourceCellType"},
+      {data: "sourceCellAnatomy"},
+      {data: "sourceSpecies"},
+      {data: "sourceDisease"}
+    ]
   });
 })
